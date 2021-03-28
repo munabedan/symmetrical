6bf3f8cc6bd5dad8ab07219a6e63146e914b3e7f
muna bedan <munabedan@gmail.com>
2021-03-29 00:24:21 +0300

    Added an app bar with title

---
+++ src/Appbar/Appbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Appbar.css';
+
+function Appbar() {
+  return (
+    <div className="Appbarroot">
+        
+        <h2>Blend</h2>
+    </div>
+  );
+}
+
+export default Appbar;
