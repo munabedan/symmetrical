8c67e90bf60926bdbaf5b5ce0cfa4f6be02d6ceb
munabedan <munabedan@gmail.com>
2021-04-19 07:10:02 +0300

    Add appbar to course page
    Show course name and course code as the title of the page

--- src/Coursepage/Appbar/Appbar.js
+++ src/Coursepage/Appbar/Appbar.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import './Appbar.css';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+      height:120
+  },
+ 
+}));
 
 function Appbar() {
+
+  const classes = useStyles();
+
+
   return (
-    <div className="Appbar">
-        
-        <h3>heat transfer</h3>
-        <h4>cosc 302</h4>
+    <div className={classes.root}>
+      <Grid container direction="column" justify="center" alignItems="center" spacing={2}>
+        <Grid item xs={10}>
+
+          <Typography variant="h5">
+            Heat Transfer
+        </Typography>
+
+
+        </Grid>
+        <Grid item xs={10}>
+          <Typography variant="subtitle1">
+            cosc 302
+        </Typography>
+        </Grid>
+      </Grid >
     </div>
+
+
   );
 }
 
