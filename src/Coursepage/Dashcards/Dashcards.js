53bee7f163926cab0b7f45f279df419deb977917
munabedan <munabedan@gmail.com>
2021-04-19 07:12:07 +0300

    Add dashboard cards to Course page
    Display a summary of course information

---
+++ src/Coursepage/Dashcards/Dashcards.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        //height: 100,
+        //width: 100,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+
+function Dashcards() {
+
+    const classes = useStyles();
+
+
+    return (
+        
+
+
+        <Grid direction="row" container spacing={1}>
+            <Grid item xs={4} >
+                <Paper className={classes.paper}>
+                    <Grid direction="column" justify="center" alignItems="center" spacing={2} container>
+                        <Grid item xs={10}>
+                            <Typography variant="h5" >
+                                Hello
+                                </Typography>
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Typography variant="h6">
+                                Hello
+                                </Typography>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+            <Grid item xs={4}>
+                <Paper className={classes.paper}>
+                    <Grid direction="column" justify="center" alignItems="center" spacing={2} container>
+                        <Grid item xs={10}>
+                            <Typography variant="h5" >
+                                Hello
+                                </Typography>
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Typography variant="h6">
+                                Hello
+                                </Typography>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+            <Grid item xs={4}>
+                <Paper className={classes.paper}>
+                    <Grid direction="column" justify="center" alignItems="center" spacing={2} container>
+                        <Grid item xs={10}>
+                            <Typography variant="h5" >
+                                Hello
+                                </Typography>
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Typography variant="h6">
+                                Hello
+                                </Typography>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+        </Grid>
+
+    )
+}
+
+export default Dashcards
