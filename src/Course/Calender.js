df73922e7fdbf18a51416035a07220359bdf6c48
munabedan <munabedan@gmail.com>
2021-04-26 06:27:13 +0300

    add styling for absent and attended dates

--- src/Course/Calender.js
+++ src/Course/Calender.js
@@ -3,10 +3,9 @@ import React, { useState } from 'react';
 import MomentUtils from '@date-io/moment';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { makeStyles } from '@material-ui/core/styles';
-
 const useStyles = makeStyles((theme) => ({
-  
 
+  //normal days
   day: {
     width: 36,
     height: 36,
@@ -14,17 +13,37 @@ const useStyles = makeStyles((theme) => ({
     margin: "0 2px",
     color: "inherit",
   },
-  content:{
-    width:'100%',
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
+  content: {
+    width: '100%',
+    height:'100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  date:{
-    fontFamily:'serif',
-    fontWeight:400,
-    lineHeight:1.43,
-    color:'inherit',
+  date: {
+    fontFamily: 'serif',
+    fontWeight: 400,
+    lineHeight: 1.43,
+    color: 'inherit',
+  },
+  //attended days
+  dateAttended: {
+    fontFamily: 'serif',
+    fontWeight: 400,
+    lineHeight: 1.43,
+    color: '#74CEF6',
+  },
+  //missed days
+  dateMissed: {
+    width: 36,
+    height: 36,
+    margin: "0 2px",
+    
+    background: 'linear-gradient(45deg, #Ff8664 10%, #FF4965 90%)',
+    color: 'white',
+    borderRadius: '50%',
+    transform: 'scale(0.75)',
+
   },
 }));
 
@@ -32,18 +51,26 @@ export default function Calender() {
   const [selectedDate, handleDateChange] = useState(new Date());
   const classes = useStyles();
   function renderDay(day) {
-    return(
+    return (
       <div>
-          { day.date() === 0 ? <img src=""></img> 
+        { day.date() === 15 ?
+          <div className={classes.day}>
+            <span className={classes.content}>
+              <p className={classes.dateAttended}>
+                {day.date()}
+              </p>
+            </span>
+          </div>
+
           :
-              <div className={classes.day}>
-                <span className={classes.content}>
-                  <p className={classes.date}>
-                    {day.date()}
-                  </p>
-                </span>
-              </div>
-          }
+          <div className={classes.dateMissed}>
+            <span className={classes.content}>
+              <p className={classes.date}>
+                {day.date()}
+              </p>
+            </span>
+          </div>
+        }
       </div>
     );
   }
