79de83a20eda38b0366c436b4950e98350c8f4d1
munabedan <munabedan@gmail.com>
2021-04-26 05:40:11 +0300

    create custom calendar day elements
    this  will alow the custom styling of dates in calendar

--- src/Course/Calender.js
+++ src/Course/Calender.js
@@ -7,12 +7,46 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   
 
+  day: {
+    width: 36,
+    height: 36,
+    fontSize: theme.typography.caption.fontSize,
+    margin: "0 2px",
+    color: "inherit",
+  },
+  content:{
+    width:'100%',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  date:{
+    fontFamily:'serif',
+    fontWeight:400,
+    lineHeight:1.43,
+    color:'inherit',
+  },
 }));
 
 export default function Calender() {
   const [selectedDate, handleDateChange] = useState(new Date());
   const classes = useStyles();
-
+  function renderDay(day) {
+    return(
+      <div>
+          { day.date() === 0 ? <img src=""></img> 
+          :
+              <div className={classes.day}>
+                <span className={classes.content}>
+                  <p className={classes.date}>
+                    {day.date()}
+                  </p>
+                </span>
+              </div>
+          }
+      </div>
+    );
+  }
   return (
 
 
@@ -21,6 +55,7 @@ export default function Calender() {
       <DatePicker
         value={selectedDate}
         onChange={handleDateChange}
+        renderDay={renderDay}
         size="small"
         variant="static"
         orientation="landscape"
