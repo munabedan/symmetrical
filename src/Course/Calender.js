e039c91ec567091dc2061abfe6cb0e272b9f54aa
munabedan <munabedan@gmail.com>
2021-04-26 05:13:36 +0300

    add styling capability to calendar component

--- src/Course/Calender.js
+++ src/Course/Calender.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 
 import MomentUtils from '@date-io/moment';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  
+
+}));
 
 export default function Calender() {
   const [selectedDate, handleDateChange] = useState(new Date());
+  const classes = useStyles();
 
   return (
 
